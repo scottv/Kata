cfdd5a10476bd644fcda7de6bae02c897b4288bf
unknown <svickers@sunflower.com>
2013-04-17 20:03:33 -0500

    Adding new solution

---
+++ Kata/ClassLibrary1/Set.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Set
+    {
+        public void Add(object s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ Kata/KataTests/SetTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+using NUnit.Framework;
+
+namespace KataTests
+{
+    [TestFixture()]
+    public class SetTests
+    {
+        [Test()]
+        public void CanAddAnItemToSet()
+        {
+            Set monkey = new Set();
+            
+            Assert.DoesNotThrow(()=>monkey.Add("1"));
+    
+        }
+    }
+}
